4146267a138787329c9d6e6cd9b3897ef82bd41d
jfroberto <jroberto.xiii@gmail.com>
2020-04-19 11:33:24 +0800

    add to git

---
+++ app.js
@@ -0,0 +1,363 @@
+function keyCode(event){
+    //this is var for names
+    var	c001 = names001.value;	var	c002 = names002.value;	var	c003 = names003.value;	var	c004 = names004.value;	var	c005 = names005.value;	var	c006 = names006.value;	var	c007 = names007.value;	var	c008 = names008.value;	var	c009 = names009.value;	var	c010 = names010.value;	var	c011 = names011.value;	var	c012 = names012.value; var	c013 = names013.value;	var	c014 = names014.value;
+    var	c015 = names015.value;	var	c016 = names016.value;	var	c017 = names017.value;	var	c018 = names018.value;	var	c019 = names019.value;	var	c020 = names020.value;	var	c021 = names021.value;	var	c022 = names022.value;	var	c023 = names023.value;	var	c024 = names024.value;	var	c025 = names025.value;	var	c026 = names026.value;	var	c027 = names027.value;	var	c028 = names028.value;
+    var	c029 = names029.value;	var	c030 = names030.value;	var	c031 = names031.value;	var	c032 = names032.value;	var	c033 = names033.value;	var	c034 = names034.value;	var	c035 = names035.value;	var	c036 = names036.value;	var	c037 = names037.value;	var	c038 = names038.value;	var	c039 = names039.value;	var	c040 = names040.value;	var	c041 = names041.value;	var	c042 = names042.value;
+    var	c043 = names043.value;	var	c044 = names044.value;	var	c045 = names045.value;	var	c046 = names046.value;	var	c047 = names047.value;	var	c048 = names048.value;	var	c049 = names049.value;	var	c050 = names050.value;	var	c051 = names051.value;	var	c052 = names052.value;	var	c053 = names053.value;	var	c054 = names054.value;	var	c055 = names055.value;	var	c056 = names056.value;
+    var	c057 = names057.value;	var	c058 = names058.value;	var	c059 = names059.value;	var	c060 = names060.value;	var	c061 = names061.value;	var	c062 = names062.value;	var	c063 = names063.value;	var	c064 = names064.value;	var	c065 = names065.value;	var	c066 = names066.value;	var	c067 = names067.value;	var	c068 = names068.value;	var	c069 = names069.value;	var	c070 = names070.value;
+    var	c071 = names071.value;	var	c072 = names072.value;	var	c073 = names073.value;	var	c074 = names074.value;	var	c075 = names075.value;	var	c076 = names076.value;	var	c077 = names077.value;	var	c078 = names078.value;	var	c079 = names079.value;	var	c080 = names080.value;	var	c081 = names081.value;	var	c082 = names082.value;	var	c083 = names083.value;	var	c084 = names084.value;
+    var	c085 = names085.value;	var	c086 = names086.value;	var	c087 = names087.value;	var	c088 = names088.value;	var	c089 = names089.value;	var	c090 = names090.value;	var	c091 = names091.value;	var	c092 = names092.value;	var	c093 = names093.value;	var	c094 = names094.value;	var	c095 = names095.value;	var	c096 = names096.value;	var	c097 = names097.value;	var	c098 = names098.value;
+    var	c099 = names099.value;	var	c100 = names100.value;	var	c101 = names101.value;	var	c102 = names102.value;	var	c103 = names103.value;	var	c104 = names104.value;	var	c105 = names105.value;	var	c106 = names106.value;	var	c107 = names107.value;	var	c108 = names108.value;	var	c109 = names109.value;	var	c110 = names110.value;	var	c111 = names111.value;	var	c112 = names112.value;
+    var	c113 = names113.value;	var	c114 = names114.value;	var	c115 = names115.value;	var	c116 = names116.value;	var	c117 = names117.value;	var	c118 = names118.value;	var	c119 = names119.value;	var	c120 = names120.value;	var	c121 = names121.value;	var	c122 = names122.value;	var	c123 = names123.value;	var	c124 = names124.value;	var	c125 = names125.value;	var	c126 = names126.value;
+    var	c127 = names127.value;	var	c128 = names128.value;	var	c129 = names129.value;	var	c130 = names130.value;	var	c131 = names131.value;	var	c132 = names132.value;	var	c133 = names133.value;	var	c134 = names134.value;	var	c135 = names135.value;	var	c136 = names136.value;	var	c137 = names137.value;	var	c138 = names138.value;	var	c139 = names139.value;	var	c140 = names140.value;
+    var	c141 = names141.value;	var	c142 = names142.value;	var	c143 = names143.value;	var	c144 = names144.value;	var	c145 = names145.value;	var	c146 = names146.value;	var	c147 = names147.value;	var	c148 = names148.value;	var	c149 = names149.value;	var	c150 = names150.value;	var	c151 = names151.value;	var	c152 = names152.value;	var	c153 = names153.value;	var	c154 = names154.value;
+    var	c155 = names155.value;	var	c156 = names156.value;	var	c157 = names157.value;	var	c158 = names158.value;	var	c159 = names159.value;	var	c160 = names160.value;	var	c161 = names161.value;	var	c162 = names162.value;	var	c163 = names163.value;	var	c164 = names164.value;	var	c165 = names165.value;	var	c166 = names166.value;	var	c167 = names167.value;	var	c168 = names168.value;
+    var	c169 = names169.value;	var	c170 = names170.value;	var	c171 = names171.value;	var	c172 = names172.value;	var	c173 = names173.value;	var	c174 = names174.value;	var	c175 = names175.value;	var	c176 = names176.value;	var	c177 = names177.value;	var	c178 = names178.value;	var	c179 = names179.value;	var	c180 = names180.value;	var	c181 = names181.value;	var	c182 = names182.value;
+    var	c183 = names183.value;	var	c184 = names184.value;	var	c185 = names185.value;	var	c186 = names186.value;	var	c187 = names187.value;	var	c188 = names188.value;	var	c189 = names189.value;	var	c190 = names190.value;	var	c191 = names191.value;	var	c192 = names192.value;	var	c193 = names193.value;	var	c194 = names194.value;	var	c195 = names195.value;	var	c196 = names196.value;
+
+    //this is for the letter
+    var l001 = 0; var l002 = 0; var l003 = 0; var l004 = 0; var l005 = 0; var l006 = 0;	var l007 = 0; var l008 = 0;	var l009 = 0; var l010 = 0;	var l011 = 0; var l012 = 0;	var l013 = 0; var l014 = 0;
+    var l015 = 0; var l016 = 0; var l017 = 0; var l018 = 0; var l019 = 0; var l020 = 0;	var l021 = 0; var l022 = 0;	var l023 = 0; var l024 = 0;	var l025 = 0; var l026 = 0;	var l027 = 0; var l028 = 0;
+    var l029 = 0; var l030 = 0; var l031 = 0; var l032 = 0; var l033 = 0; var l034 = 0;	var l035 = 0; var l036 = 0;	var l037 = 0; var l038 = 0;	var l039 = 0; var l040 = 0;	var l041 = 0; var l042 = 0;
+    var l043 = 0; var l044 = 0; var l045 = 0; var l046 = 0; var l047 = 0; var l048 = 0;	var l049 = 0; var l050 = 0;	var l051 = 0; var l052 = 0;	var l053 = 0; var l054 = 0;	var l055 = 0; var l056 = 0;
+    var l057 = 0; var l058 = 0; var l059 = 0; var l060 = 0; var l061 = 0; var l062 = 0;	var l063 = 0; var l064 = 0;	var l065 = 0; var l066 = 0;	var l067 = 0; var l068 = 0;	var l069 = 0; var l070 = 0;
+    var l071 = 0; var l072 = 0; var l073 = 0; var l074 = 0; var l075 = 0; var l076 = 0;	var l077 = 0; var l078 = 0;	var l079 = 0; var l080 = 0;	var l081 = 0; var l082 = 0;	var l083 = 0; var l084 = 0;
+    var l085 = 0; var l086 = 0; var l087 = 0; var l088 = 0; var l089 = 0; var l090 = 0;	var l091 = 0; var l092 = 0;	var l093 = 0; var l094 = 0;	var l095 = 0; var l096 = 0;	var l097 = 0; var l098 = 0;
+    var l099 = 0; var l100 = 0; var l101 = 0; var l102 = 0; var l103 = 0; var l104 = 0;	var l105 = 0; var l106 = 0;	var l107 = 0; var l108 = 0;	var l109 = 0; var l110 = 0;	var l111 = 0; var l112 = 0;
+    var l113 = 0; var l114 = 0; var l115 = 0; var l116 = 0; var l117 = 0; var l118 = 0;	var l119 = 0; var l120 = 0;	var l121 = 0; var l122 = 0;	var l123 = 0; var l124 = 0;	var l125 = 0; var l126 = 0;
+    var l127 = 0; var l128 = 0; var l129 = 0; var l130 = 0; var l131 = 0; var l132 = 0;	var l133 = 0; var l134 = 0;	var l135 = 0; var l136 = 0;	var l137 = 0; var l138 = 0;	var l139 = 0; var l140 = 0;
+    var l141 = 0; var l142 = 0; var l143 = 0; var l144 = 0; var l145 = 0; var l146 = 0;	var l147 = 0; var l148 = 0;	var l149 = 0; var l150 = 0;	var l151 = 0; var l152 = 0;	var l153 = 0; var l154 = 0;
+    var l155 = 0; var l156 = 0; var l157 = 0; var l158 = 0; var l159 = 0; var l160 = 0;	var l161 = 0; var l162 = 0;	var l163 = 0; var l164 = 0;	var l165 = 0; var l166 = 0;	var l167 = 0; var l168 = 0;
+    var l169 = 0; var l170 = 0; var l171 = 0; var l172 = 0; var l173 = 0; var l174 = 0;	var l175 = 0; var l176 = 0;	var l177 = 0; var l178 = 0;	var l179 = 0; var l180 = 0;	var l181 = 0; var l182 = 0;
+    var l183 = 0; var l184 = 0; var l185 = 0; var l186 = 0; var l187 = 0; var l188 = 0;	var l189 = 0; var l190 = 0;	var l191 = 0; var l192 = 0;	var l193 = 0; var l194 = 0;	var l195 = 0; var l196 = 0;
+
+
+    
+
+    var x = event.keyCode;
+
+    if (x == 87  &&  c012 == "わ") {
+        l001 = 1;
+        document.getElementById("names012").value = c012;
+    }
+    
+    if (x == "&‌#x305F"  && c013 == "た") {
+        l002 = 1;
+        document.getElementById("names013").value = c013; 
+    }
+    if (x == 13 && c014 == "し") {
+        l003 = 1;
+        document.getElementById("names014").value = c014; 
+    }
+
+    if (x == 13 && c020 == "は") {
+        l004 = 1;
+        document.getElementById("names014").value = c020; 
+    }
+
+    if (x == 13 && c021 == "い") {
+        l005 = 1;
+        document.getElementById("names014").value = c021; 
+    }
+
+    //な	ん	さ	い
+
+    if (x == 13 && c053 == "な") {
+        l006 = 1;
+        document.getElementById("names014").value = c053; 
+    }
+
+    if (x == 13 && c054 == "ん") {
+        l007 = 1;
+        document.getElementById("names014").value = c054; 
+    }
+
+    if (x == 13 && c055 == "さ") {
+        l008 = 1;
+        document.getElementById("names014").value = c055; 
+    }
+
+    if (x == 13 && c056 == "さ") {
+        l009 = 1;
+        document.getElementById("names014").value = c056; 
+    }
+
+    //ど	う	ぞ	よ	ろ	し	く	お	ね	が	い	し	ま	す
+
+    if (x == 13 && c071 == "ど") {
+        l010 = 1;
+        document.getElementById("names014").value = c071; 
+    }
+
+    if (x == 13 && c072 == "う") {
+        l011 = 1;
+        document.getElementById("names014").value = c072; 
+    }
+
+    if (x == 13 && c073 == "ぞ") {
+        l012 = 1;
+        document.getElementById("names014").value = c073 
+    }
+
+    if (x == 13 && c074 == "よ") {
+        l013 = 1;
+        document.getElementById("names014").value = c074; 
+    }
+
+    if (x == 13 && c075 == "ろ") {
+        l014 = 1;
+        document.getElementById("names014").value = c075; 
+    }
+
+    if (x == 13 && c076 == "し") {
+        l015 = 1;
+        document.getElementById("names014").value = c076; 
+    }
+
+    if (x == 13 && c077 == "く") {
+        l016 = 1;
+        document.getElementById("names014").value = c077; 
+    }
+
+    if (x == 13 && c078 == "お") {
+        l017 = 1;
+        document.getElementById("names014").value = c078; 
+    }
+
+    if (x == 13 && c079 == "ね" ) {
+        l018 = 1;
+        document.getElementById("names014").value = c079; 
+    }
+
+    if (x == 13 && c080 == "が") {
+        l019 = 1;
+        document.getElementById("names014").value = c080; 
+    }
+
+    if (x == 13 && c081 == "い") {
+        l020 = 1;
+        document.getElementById("names014").value = c081; 
+    }
+
+    if (x == 13 && c082 == "し") {
+        l021 = 1;
+        document.getElementById("names014").value = c082; 
+    }
+
+    if (x == 13 && c083 == "ま") {
+        l022 = 1;
+        document.getElementById("names014").value = c083; 
+    }
+
+    if (x == 13 && c084 == "す") {
+        l023 = 1;
+        document.getElementById("names014").value = c084; 
+    }
+    //い	しゃ
+    if (x == 13 && c122 == "す") {
+        l024 = 1;
+        document.getElementById("names014").value = c122; 
+    }
+    if (x == 13 && c123 == "しゃ") {
+        l025 = 1;
+        document.getElementById("names014").value = c123; 
+    }
+
+    // い	い	え
+    if (x == 13 && c137 == "い") {
+        l026 = 1;
+        document.getElementById("names014").value = c137; 
+    }
+    if (x == 13 && c138 == "い") {
+        l027 = 1;
+        document.getElementById("names014").value = c138; 
+    }
+    if (x == 13 && c139 == "え") {
+        l028 = 1;
+        document.getElementById("names014").value = c139; 
+    }
+
+    //ぎ	ん	こ	う	い	ん
+
+    if (x == 13 && c146 == "ぎ") {
+        l029 = 1;
+        document.getElementById("names014").value = c146; 
+    }
+
+    if (x == 13 && c147 == "ん") {
+        l030 = 1;
+        document.getElementById("names014").value = c147; 
+    }
+
+    if (x == 13 && c148 == "こ") {
+        l031 = 1;
+        document.getElementById("names014").value = c148; 
+    }
+
+    if (x == 13 && c149 == "う") {
+        l032 = 1;
+        document.getElementById("names014").value = c149; 
+    }
+
+    if (x == 13 && c150 == "い") {
+        l033 = 1;
+        document.getElementById("names014").value = c150; 
+    }
+
+    if (x == 13 && c151 == "ん") {
+        l034 = 1;
+        document.getElementById("names014").value = c151; 
+    }
+
+    //びょ	う	い	ん
+    if (x == 13 && c174 == "びょ") {
+        l035 = 1;
+        document.getElementById("names014").value = c174; 
+    }
+    if (x == 13 && c175 == "う") {
+        l036 = 1;
+        document.getElementById("names014").value = c175; 
+    }
+    if (x == 13 && c176 == "い") {
+        l037 = 1;
+        document.getElementById("names014").value = c176; 
+    }
+    if (x == 13 && c176 == "ん") {
+        l038 = 1;
+        document.getElementById("names014").value = c176; 
+    }
+
+    //DOWN
+    //しつれいですが
+    //if (x == 13 && c014 == "し") {
+    //    l039 = 1;
+    //    document.getElementById("names014").value = c014; 
+    //}
+    if (x == 13 && c028 == "つ") {
+        l040 = 1;
+        document.getElementById("names014").value = c028; 
+    }
+    if (x == 13 && c042 == "れ") {
+        l041 = 1;
+        document.getElementById("names014").value = c042; 
+    }
+   // if (x == 13 && c056 == "い") {
+   //     l042 = 1;
+   //     document.getElementById("names014").value = c056; 
+   // }
+    if (x == 13 && c070 == "で") {
+        l043 = 1;
+        document.getElementById("names014").value = c070; 
+    }
+   // if (x == 13 && c084 == "す") {
+   //     l044 = 1;
+   //     document.getElementById("names014").value = c084; 
+   // }
+    if (x == 13 && c098 == "が") {
+        l045 = 1;
+        document.getElementById("names014").value = c098; 
+    }
+
+    //はじめまして
+    if (x == 13 && c020 == "は") {
+        l046 = 1;
+        document.getElementById("names014").value = c020; 
+    }
+    if (x == 13 && c034 == "じ") {
+        l047 = 1;
+        document.getElementById("names014").value = c034; 
+    }
+    if (x == 13 && c048 == "め") {
+        l048 = 1;
+        document.getElementById("names014").value = c048; 
+    }
+    if (x == 13 && c062 == "ま") {
+        l049 = 1;
+        document.getElementById("names014").value = c062; 
+    }
+    if (x == 13 && c076 == "し") {
+        l050 = 1;
+        document.getElementById("names014").value = c076; 
+    }
+    if (x == 13 && c090 == "て") {
+        l051 = 1;
+        document.getElementById("names014").value = c090; 
+    }
+
+    //きょうし
+    if (x == 13 && c058 == "きょ") {
+        l052 = 1;
+        document.getElementById("names014").value = c058; 
+    }
+    if (x == 13 && c072 == "う") {
+        l053 = 1;
+        document.getElementById("names014").value = c072; 
+    }
+    if (x == 13 && c086 == "し") {
+        l054 = 1;
+        document.getElementById("names014").value = c086; 
+    }
+
+    //がくせい
+    if (x == 13 && c080 == "が") {
+        l055 = 1;
+        document.getElementById("names014").value = c080; 
+    }
+    if (x == 13 && c094 == "く") {
+        l056 = 1;
+        document.getElementById("names014").value = c094; 
+    }
+    if (x == 13 && c108 == "せ") {
+        l057 = 1;
+        document.getElementById("names014").value = c108; 
+    }
+    if (x == 13 && c122 == "い") {
+        l058 = 1;
+        document.getElementById("names014").value = c122; 
+    }
+
+    //けんきゅうしゃ 
+    if (x == 13 && c133 == "け") {
+        l059 = 1;
+        document.getElementById("names014").value = c133; 
+    }
+    if (x == 13 && c147 == "ん") {
+        l060 = 1;
+        document.getElementById("names014").value = c147; 
+    }
+    if (x == 13 && c161 == "きゅ") {
+        l061 = 1;
+        document.getElementById("names014").value = c161; 
+    }
+    if (x == 13 && c175 == "う") {
+        l062 = 1;
+        document.getElementById("names014").value = c175; 
+    }
+    if (x == 13 && c189 == "しゃ") {
+        l063 = 1;
+        document.getElementById("names014").value = c189; 
+    }
+
+    //しゃいん
+    if (x == 13 && c123 == "しゃ") {
+        l064 = 1;
+        document.getElementById("names014").value = c123; 
+    }
+    if (x == 13 && c137 == "い") {
+        l065 = 1;
+        document.getElementById("names014").value = c137; 
+    }
+    if (x == 13 && c151 == "ん") {
+        l066 = 1;
+        document.getElementById("names014").value = c151; 
+    }
+
+
+    var k = l001	+	l002	+	l003	+	l004	+	l005	+	l006	+	l007	+	l008	+	l009	+	l010	+	l011	+	l012	+	l013	+	l014	+	l015	+	l016	+	l017	+	l018	+	l019	+	l020	+	l021	+	l022	+	l023	+	l024	+	l025	+	l026	+	l027	+	l028	+	l029	+	l030	+	l031	+	l032	+	l033	+	l034	+	l035	+	l036	+	l037	+	l038	+	l039	+	l040	+	l041	+	l042	+	l043	+	l044	+	l045	+	l046	+	l047	+	l048	+	l049	+	l050	+	l051	+	l052	+	l053	+	l054	+	l055	+	l056	+	l057	+	l058	+	l059	+	l060	+	l061	+	l062	+	l063	+	l064	+	l065	+	l066	+	l067	+	l068	+	l069	+	l070;
+
+    document.getElementById("score001").innerHTML = "Number of letters:  " + k + " out of 60";
+    if (k == "60") {
+        document.getElementById("next001").innerHTML = "Congratulation You solve the puzzle";
+    }
+    
+}
